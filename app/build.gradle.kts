c4192ff36b198624cfc9229e6543dcbff63a17b8
Rychkov Alexander <magic.goop1993@gmail.com>
2025-09-15 14:49:38 +0300

    project setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,46 +1,26 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("build-logic.application")
 }
 
 android {
-    namespace = "dev.aleksrychkov.scrooge"
-    compileSdk = 36
-
-    defaultConfig {
-        applicationId = "dev.aleksrychkov.scrooge"
-        minSdk = 26
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    buildFeatures {
+        buildConfig = true
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+        }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.android.material)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,10 @@
+@file:Suppress("UnstableApiUsage")
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 pluginManagement {
+    includeBuild("android-build-logic")
+
     repositories {
         google {
             content {
