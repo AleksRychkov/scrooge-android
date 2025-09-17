ea62d711015f712f9c6d2406f034dc93faad6f8e
Rychkov Alexander <magic.goop1993@gmail.com>
2025-09-17 19:54:28 +0300

    detekt to separate gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,22 +10,3 @@ allprojects {
         useJUnitPlatform()
     }
 }
-
-// todo: move to build logic as separate plugin?:
-// https://github.com/LinX64/CoinCap/pull/26/files#diff-8cff73265af19c059547b76aca8882cbaa3209291406f52df1dafbbc78e80c46
-val detektFormatting = libs.detekt.formatting
-subprojects {
-    apply {
-        plugin("io.gitlab.arturbosch.detekt")
-    }
-
-    detekt {
-        config.from(rootProject.files("config/detekt/detekt.yml"))
-        buildUponDefaultConfig = true
-        parallel = true
-    }
-
-    dependencies {
-        detektPlugins(detektFormatting)
-    }
-}
