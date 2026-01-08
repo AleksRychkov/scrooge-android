822e24e11a122f4be5c66c1cc1f0eaa4ee8f5b49
Rychkov Alexander <magic.goop1993@gmail.com>
2026-01-08 13:42:01 +0200

    change export file name

--- feature/transfer/default/src/main/kotlin/dev/aleksrychkov/scrooge/feature/transfer/internal/DefaultExportDataUseCase.kt
+++ feature/transfer/default/src/main/kotlin/dev/aleksrychkov/scrooge/feature/transfer/internal/DefaultExportDataUseCase.kt
@@ -10,6 +10,9 @@ import androidx.work.WorkManager
 import dev.aleksrychkov.scrooge.feature.transfer.ExportDataUseCase
 import dev.aleksrychkov.scrooge.feature.transfer.GetExportUriUseCase
 import dev.aleksrychkov.scrooge.feature.transfer.internal.workers.ExportWorker
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
 
 internal class DefaultExportDataUseCase(
     private val exportUriUseCase: Lazy<GetExportUriUseCase>,
@@ -17,11 +20,13 @@ internal class DefaultExportDataUseCase(
 ) : ExportDataUseCase {
 
     private companion object {
-        const val DB_BACKUP_NAME = "scrooge_backup.db"
+        const val DB_BACKUP_NAME = "scrooge_backup"
     }
 
     override suspend fun invoke() {
-        val uriString = exportUriUseCase.value.invoke(DB_BACKUP_NAME)
+        val dt = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).toString()
+        val dbName = DB_BACKUP_NAME + "_" + dt
+        val uriString = exportUriUseCase.value.invoke(dbName)
         checkNotNull(uriString)
         val data = Data.Builder().put(ExportWorker.KEY_URI, uriString).build()
         val request = OneTimeWorkRequestBuilder<ExportWorker>()
