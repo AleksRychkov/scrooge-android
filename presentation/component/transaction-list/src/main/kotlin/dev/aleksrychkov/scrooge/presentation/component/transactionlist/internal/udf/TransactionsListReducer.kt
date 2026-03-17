579eba20610aaa889c07c4b15618a8751cec7713
Rychkov Alexander <magic.goop1993@gmail.com>
2026-03-17 12:29:01 +0300

    transactions list date references

--- presentation/component/transaction-list/src/main/kotlin/dev/aleksrychkov/scrooge/presentation/component/transactionlist/internal/udf/TransactionsListReducer.kt
+++ presentation/component/transaction-list/src/main/kotlin/dev/aleksrychkov/scrooge/presentation/component/transactionlist/internal/udf/TransactionsListReducer.kt
@@ -4,21 +4,34 @@ import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import androidx.paging.insertSeparators
 import androidx.paging.map
+import dev.aleksrychkov.scrooge.core.di.get
 import dev.aleksrychkov.scrooge.core.entity.TransactionEntity
+import dev.aleksrychkov.scrooge.core.entity.readableName
+import dev.aleksrychkov.scrooge.core.resources.ResourceManager
 import dev.aleksrychkov.scrooge.core.udf.Reducer
 import dev.aleksrychkov.scrooge.core.udf.ReducerResult
 import dev.aleksrychkov.scrooge.core.udf.reduceWith
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import dev.aleksrychkov.scrooge.core.resources.R as Resources
 
 internal class TransactionsListReducer(
     private val scope: CoroutineScope,
+    private val resourceManager: ResourceManager = get(),
 ) :
     Reducer<TransactionsListState, TransactionsListEvent, TransactionsListCommand, Unit> {
 
     private val mapper: TransactionsListMapper by lazy {
         TransactionsListMapper()
+    }
+
+    private val dateToday: String by lazy {
+        resourceManager.getString(Resources.string.today)
+    }
+
+    private val dateYesterday: String by lazy {
+        resourceManager.getString(Resources.string.yesterday)
     }
 
     override fun reduce(
@@ -49,7 +62,10 @@ internal class TransactionsListReducer(
                         val transactions = mutableMapOf<String, MutableList<TransactionEntity>>()
                         pagingData
                             .map { entity ->
-                                val itemDate = entity.datestamp.readableName()
+                                val itemDate = entity.datestamp.readableName(
+                                    today = dateToday,
+                                    yesterday = dateYesterday,
+                                )
                                 val map = transactions[itemDate] ?: mutableListOf()
                                 map.add(entity)
                                 transactions[itemDate] = map
