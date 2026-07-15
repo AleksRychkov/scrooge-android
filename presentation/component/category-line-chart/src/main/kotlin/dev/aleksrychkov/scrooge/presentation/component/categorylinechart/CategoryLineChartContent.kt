c7618af12dbaf9e10cb820501bc7959a7d2fe485
Rychkov Alexander <magic.goop1993@gmail.com>
2026-07-15 23:30:24 +0300

    improve category chart legend action

--- presentation/component/category-line-chart/src/main/kotlin/dev/aleksrychkov/scrooge/presentation/component/categorylinechart/CategoryLineChartContent.kt
+++ presentation/component/category-line-chart/src/main/kotlin/dev/aleksrychkov/scrooge/presentation/component/categorylinechart/CategoryLineChartContent.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -22,8 +23,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -46,6 +49,8 @@ import com.patrykandpatrick.vico.compose.common.component.rememberTextComponent
 import com.patrykandpatrick.vico.compose.common.data.ExtraStore
 import dev.aleksrychkov.scrooge.core.designsystem.theme.AppTheme
 import dev.aleksrychkov.scrooge.core.designsystem.theme.Large
+import dev.aleksrychkov.scrooge.core.designsystem.theme.Medium
+import dev.aleksrychkov.scrooge.core.designsystem.theme.Normal
 import dev.aleksrychkov.scrooge.core.utils.formatCompactNumber
 import dev.aleksrychkov.scrooge.presentation.component.categorylinechart.internal.CategoryLineChartComponentInternal
 import dev.aleksrychkov.scrooge.presentation.component.categorylinechart.internal.udf.CategoryLineChartState
@@ -125,9 +130,13 @@ private fun ChartWithLegend(
                         Modifier.size(8.dp).background(Color(series.color), CircleShape),
                     )
                     Text(
-                        modifier = Modifier.clickable(onClick = openCategoryFilter),
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(Normal))
+                            .clickable(onClick = openCategoryFilter)
+                            .padding(horizontal = Normal, vertical = Medium),
                         text = series.name,
                         style = MaterialTheme.typography.labelMedium,
+                        textDecoration = TextDecoration.Underline,
                     )
                 }
             }
